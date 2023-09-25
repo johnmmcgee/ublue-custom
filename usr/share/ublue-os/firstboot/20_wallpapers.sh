e5fb5adec606bb6c0e6032938ba2843285bb11d4
John McGee <jmcgee@kidraye.com>
2023-09-25 01:57:19 -0400

    fix wallpapers

--- usr/share/ublue-os/firstboot/20_wallpapers.sh
+++ usr/share/ublue-os/firstboot/20_wallpapers.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
+custom_file="/usr/share/gnome-background-properties/custom.xml"
+output_file="/tmp/custom.xml"
 
-echo "Create XML file so that we can see the imported wallpapers in GNOME
-"
-OUTPUT_FILE="/usr/share/gnome-background-properties/custom.xml"
+echo "Create XML file so that we can see the imported wallpapers in GNOME"
 
-echo '<?xml version="1.0" encoding="UTF-8"?>' > "$OUTPUT_FILE"
-echo '<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">' >> "$OUTPUT_FILE"
-echo '<wallpapers>' >> "$OUTPUT_FILE"
+echo '<?xml version="1.0" encoding="UTF-8"?>' > "$output_file"
+echo '<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">' >> "$output_file"
+echo '<wallpapers>' >> "$output_file"
 
 for file in /usr/share/backgrounds/custom/*; do
     if [ -f "$file" ]; then
 
         filename=$(basename "$file")
 
-        echo '  <wallpaper>' >> "$OUTPUT_FILE"
-        echo "    <name>${filename%.*}</name>" >> "$OUTPUT_FILE"
-        echo "    <filename>$file</filename>" >> "$OUTPUT_FILE"
-        echo '    <options>zoom</options>' >> "$OUTPUT_FILE"
-        echo '    <pcolor>#000000</pcolor>' >> "$OUTPUT_FILE"
-        echo '    <scolor>#000000</scolor>' >> "$OUTPUT_FILE"
-        echo '    <shade_type>solid</shade_type>' >> "$OUTPUT_FILE"
-        echo '  </wallpaper>' >> "$OUTPUT_FILE"
+        echo '  <wallpaper>' >> "$output_file"
+        echo "    <name>${filename%.*}</name>" >> "$output_file"
+        echo "    <filename>$file</filename>" >> "$output_file"
+        echo '    <options>zoom</options>' >> "$output_file"
+        echo '    <pcolor>#000000</pcolor>' >> "$output_file"
+        echo '    <scolor>#000000</scolor>' >> "$output_file"
+        echo '    <shade_type>solid</shade_type>' >> "$output_file"
+        echo '  </wallpaper>' >> "output_file"
     fi
 done
 
-echo '</wallpapers>' >> "$OUTPUT_FILE"
+echo '</wallpapers>' >> "$output_file"
 
-echo "Custom wallpapers XML generated at $OUTPUT_FILE"
+sudo mv "$output_file" "$custom_file"
+
+echo "Custom wallpapers XML generated at $output_file"
