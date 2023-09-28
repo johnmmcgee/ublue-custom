1df3d2dfef9daa39b3d370a92e09e98e7a40a43c
John McGee <jmcgee@kidraye.com>
2023-09-27 22:46:39 -0400

    clean up

--- usr/share/ublue-os/firstboot/10_flatpaks.sh
+++ usr/share/ublue-os/firstboot/10_flatpaks.sh
@@ -4,35 +4,35 @@ read -p "Do you want to install all system flatpaks? (y/n): " choice
 if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
     echo "Disable all repos but flatub for this install .. "
-    sudo /usr/lib/fedora-third-party/fedora-third-party-opt-out
-    sudo /usr/bin/fedora-third-party disable
-    
+    flatpak remote-delete fedora --force
+    flatpak remote-delete fedora-testing --force
+
     read -p "Do you want to clean and remove all current system flatpaks? (recommended for first run) (y/n): " choice
     if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
-      flatpak remote-delete fedora --force
       flatpak remove --system --noninteractive --all
-    
+
     fi
     
     #sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
 
     echo "Installing flatpaks that we wish to be system-wide..."
     echo "Communication apps ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       com.discordapp.Discord \
       com.slack.Slack \
       org.signal.Signal \
       org.telegram.desktop
 
     echo "Gaming applications ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       com.mojang.Minecraft \
       com.valvesoftware.Steam
 
     echo "Gnome applications ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       org.fedoraproject.MediaWriter \
       org.gnome.baobab \
       org.gnome.Calculator \
@@ -51,25 +51,25 @@ if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
       com.mattjakeman.ExtensionManager
 
     echo "Internet applications ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       com.github.micahflee.torbrowser-launcher \
       org.mozilla.firefox
 
     echo "Multimeda applications ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       com.spotify.Client \
       org.videolan.VLC
 
     echo "Productivity applications ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       org.libreoffice.LibreOffice 
   
     echo "Utilities ..."
-    sudo flatpak install -y --system \
+    flatpak install -y --system \
       net.cozic.joplin_desktop \
       org.keepassxc.KeePassXC
 
     echo "Re-enable repos"
-    sudo /usr/bin/fedora-third-party enable
+    /usr/bin/fedora-third-party enable
 
 fi
