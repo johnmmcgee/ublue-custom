d70528143b7b6bc319720cf7c05ed094b43e5942
John McGee <jmcgee@kidraye.com>
2023-10-07 10:15:24 -0400

    shift things around with firstboot

--- usr/etc/profile.d/ublue-firstboot.sh
+++ usr/etc/profile.d/ublue-firstboot.sh
@@ -6,10 +6,11 @@ get_fedora_major_version() {
 
 firstboot_dir="$HOME/.config/ublue-os/firstboot"
 firstboot_setup_file="$firstboot_dir/firstboot-setup-done"
-firstboot_desktop="/etc/skel.d/.config/autostart/ublue-firstboot.desktop"
+firstboot_desktop="ublue-firstboot.desktop"
 firstboot_profiled="/etc/profile.d/ublue-firstboot.sh"
 last_fedora_major_file="$firstboot_dir/last-fedora-major"
 autostart_dir="$HOME/.config/autostart"
+autostart_skel="/etc/skel.d/.config/autostart"
 
 # check if we are the first user
 if [ "$(id -u)" -eq 1000 ] && [ -d "$HOME" ]; then
@@ -19,11 +20,11 @@ if [ "$(id -u)" -eq 1000 ] && [ -d "$HOME" ]; then
     current_fedora_major=$(get_fedora_major_version)
     if [ "$last_fedora_major" != "$current_fedora_major" ] || [ ! -f "$firstboot_setup_file" ]; then
         [ ! -d "$autostart_dir" ] && mkdir -p "$autostart_dir"
-        cp -f "$firstboot_desktop" "$autostart_dir"
+        cp -f "$autostart_skel/$firstboot_desktop" "$autostart_dir"
         [ ! -d "$firstboot_dir" ] && mkdir -p "$firstboot_dir"
         echo "$current_fedora_major" > "$last_fedora_major_file"
     fi
 fi
 
-# delete ourselves.
 rm -f $(basename "$0")
+rm -f $autostart_dir/$firstboot_desktop
--- usr/share/ublue-os/firstboot/90_rmfirstboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "We no longer need ublue-firstboot.desktop, so we will remove.  This will be recreated on next update..."
-
-rm -rf "$HOME/.config/autostart/ublue-firstboot.desktop"
