7a26a7fec7a310789b1eb8dac22bab09348231f2
John McGee <jmcgee@kidraye.com>
2024-03-10 21:26:06 -0400

    seperate system / user flatpaks for intial install

--- usr/share/ublue-os/firstboot/10_flatpaks_system.sh
+++ usr/share/ublue-os/firstboot/10_flatpaks_system.sh
@@ -3,7 +3,7 @@
 read -p "Do you want to install all system flatpaks? (y/n): " choice
 if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
-    echo "Disable all repos but flatub for this install .. "
+    echo "Disable all repos but flathub for this install .. "
     flatpak remote-delete fedora --force
     flatpak remote-delete fedora-testing --force
 
@@ -14,28 +14,9 @@ if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
     fi
     
-    #sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
     flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
 
     echo "Installing flatpaks that we wish to be system-wide..."
-    echo "Communication applicatons ..."
-    flatpak install -y --system \
-      com.discordapp.Discord \
-      com.slack.Slack \
-      org.signal.Signal \
-      org.telegram.desktop
-
-    echo "Devolopment applications ..."
-    flatpak install -y --system \
-      com.visualstudio.code \
-      com.visualstudio.code.tool.podman
-    flatpak override --system --filesystem=xdg-run/podman com.visualstudio.code
-
-    echo "Gaming applications ..."
-    flatpak install -y --system \
-      com.mojang.Minecraft \
-      com.valvesoftware.Steam
 
     echo "Gnome applications ..."
     flatpak install -y --system \
@@ -57,22 +38,14 @@ if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
     echo "Internet applications ..."
     flatpak install -y --system \
-      com.github.micahflee.torbrowser-launcher \
       org.mozilla.firefox
 
     echo "Multimeda applications ..."
     flatpak install -y --system \
-      com.spotify.Client \
       org.videolan.VLC
 
     echo "Productivity applications ..."
     flatpak install -y --system \
       org.libreoffice.LibreOffice 
-  
-    echo "Utilities ..."
-    flatpak install -y --system \
-      net.cozic.joplin_desktop \
-      org.keepassxc.KeePassXC \
-      com.usebottles.bottles
 
 fi
